38188510d152d2ff3ca27510fa105778a95b9347
Sottim <santosottim@gmail.com>
2024-10-29 15:35:13 +0530

    wsi-level analysis

--- test-metadata-wsi.py
+++ test-metadata-wsi.py
@@ -1,5 +1,5 @@
 import openslide
-slide = openslide.OpenSlide('/home/KutumLabGPU/Documents/santosh/CellViT/example/20190610_111_551-14_4069-15-A_Biopsy_ER_HnE_40X_pyramid.tif')
+slide = openslide.OpenSlide('./example/20190610_111_551-14_4069-15-A_Biopsy_ER_HnE_40X_pyramid.tif')
 properties = slide.properties
 print(properties)
 
